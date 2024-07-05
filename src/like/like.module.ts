371bbb9aa96dde108eb24a9e894cdd5791d97b1d
Movsar Makhmutov <m.movsar@gmail.com>
2024-07-05 22:46:49 +0300

    likes

--- src/like/like.module.ts
+++ src/like/like.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LikeController } from './like.controller';
 import { LikeService } from './like.service';
+import { LikeRepository } from '@src/like/like.repository';
 
 @Module({
   controllers: [LikeController],
-  providers: [LikeService]
+  providers: [LikeService, LikeRepository],
 })
 export class LikeModule {}
