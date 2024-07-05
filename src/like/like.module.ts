4a3d1741f1e5cae049fd59a9fea80ebba9dc1a15
Movsar Makhmutov <m.movsar@gmail.com>
2024-07-05 22:50:18 +0300

    prisma

--- src/like/like.module.ts
+++ src/like/like.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { LikeController } from './like.controller';
 import { LikeService } from './like.service';
 import { LikeRepository } from '@src/like/like.repository';
+import { PrismaService } from '@src/prisma/prisma.service';
 
 @Module({
   controllers: [LikeController],
-  providers: [LikeService, LikeRepository],
+  providers: [LikeService, LikeRepository, PrismaService],
 })
 export class LikeModule {}
